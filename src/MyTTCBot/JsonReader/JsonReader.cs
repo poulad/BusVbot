701b0f6d8e19c13efadca3ffef0283343dda5e4b
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2017-02-04 20:14:47 -0500

    Added a JsonReader to parse TTC JSON.

---
+++ src/MyTTCBot/JsonReader/JsonReader.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MyTTCBot.Models;
+namespace MyTTCBot.JsonReader
+{
+    public static class JsonReader
+    {
+        public static Station LoadJson(String filepath)
+        {
+            try
+            {
+                using (var fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string json = sr.ReadToEnd();
+                        Station station = JsonConvert.DeserializeObject<Station>(json);
+                        return station;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
